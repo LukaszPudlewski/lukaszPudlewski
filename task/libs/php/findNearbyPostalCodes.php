7a48edd598a0dff2b3e5963d041806150bed8bcc
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-30 12:31:47 +0100

    a

--- task/libs/php/findNearbyPostalCodes.php
+++ task/libs/php/findNearbyPostalCodes.php
@@ -24,7 +24,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['postalCodes'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
