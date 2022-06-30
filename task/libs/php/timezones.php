a5e78543fd372f3d083544bff628fee546e477d1
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-30 14:16:06 +0100

    a

--- task/libs/php/timezones.php
+++ task/libs/php/timezones.php
@@ -24,7 +24,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['timezone'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
