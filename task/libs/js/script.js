0dfa4753eaa0882ac72e8d518a9b66e631926d7d
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-30 11:42:15 +0100

    a

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -32,3 +32,36 @@ $('#btn1').click(function() {
     }); 
 
 });
+
+
+$('#btn2').click(function() {
+
+    $.ajax({
+        url: "libs/php/findNearbyPostalCodes.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            country: $('#selCountry').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+
+                $('#txtPostalCode').html(result['data'][0]['postalcode']);
+                $('#txtName').html(result['data'][0]['name']);
+                $('#txtAdminCode1').html(result['data'][0]['adminCode1']);
+                $('#txtAdminName2').html(result['data'][0]['adminName2']);
+                $('#txtAdminCode2').html(result['data'][0]['adminCode2']);
+                $('#txtAdminName2').html(result['data'][0]['adminName2']);
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // your error code
+        }
+    }); 
+
+});
