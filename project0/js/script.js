3401976798a5faf03ffe140d3c9b29ba1ebf41ae
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-07-05 20:41:25 +0100

    a

--- project0/js/script.js
+++ project0/js/script.js
@@ -21,6 +21,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+layerGroup = new L.LayerGroup();
+  layerGroup.addTo(map);
+
+
 // geolocation 
 /*
 if (navigator.geolocation) {
