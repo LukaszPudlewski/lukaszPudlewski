48950e64aa445615d133dd1c3d018c81ab3a428a
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-07-06 14:46:09 +0100

    a

--- project0/js/script.js
+++ project0/js/script.js
@@ -27,137 +27,71 @@ layerGroup = new L.LayerGroup();
 
 // geolocation 
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    let lng = position.coords.longitude;
-    let lat = position.coords.latitude;
-    map.setView([lat, lng], 5);
-    
-
-    $.ajax({
-        url: "php/countryCode.php",
-        type: 'POST',
-        dataType: 'json',
-        data: { 
-            lng: lng,
-            lat: lat
-        },
-        success: async function(result) {
-
-let inputLow = result.data.countryName.toLowerCase().split(' ').join('%20');
 let resultCountry;
 let resultWiki;
 let resultWeather;
 
-try {
-  resultCountry = await $.ajax({
-  url: "php/restCountries.php",
-  type: 'POST',
-  dataType: 'json',
-  data: {
-      countryName: /*$('#countryInput').val()*/ inputLow
-  },
-});
+ 
 
-} catch(error) {
-console.log(error);
-}
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let lng = position.coords.longitude;
+        let lat = position.coords.latitude;
+        map.setView([lat, lng], 5);
+        $.ajax({
+            url: "php/countryCode.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                lng: lng,
+                lat: lat
+            }
+        }).done((result) => {
+            let inputLow = result.data.countryName.toLowerCase().split(' ').join('%20');
+            continueLogic(inputLow);
+            applyCountryBorder(map, inputLow);
+          })
+        })
+        }
+        else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    
+         async function continueLogic(inputLow) {
+            resultCountry = await $.ajax({
 
-try {
-resultWiki = await $.ajax({
-url: "php/wikiLinks.php",
-type: 'POST',
-dataType: 'json',
-data: {
-    countryName: /*$('#countryInput').val()*/ inputLow
-},
-});
+                url: "php/restCountries.php",
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    countryName: inputLow
+                },
+            });
+    
+             resultWiki = await $.ajax({
 
-} catch (error) {
-console.log(error);
-}
-
-try {
-resultWeather = await $.ajax({
-url: "php/openWeather.php",
-type: 'POST',
-dataType: 'json',
-data: {
-  lat: resultCountry.data[0].latlng[0],
-  lon: resultCountry.data[0].latlng[1],
-  apiKey: /*process.env.API_KEY*/ '527a25be90efba24541cd7ca1ac87e7e'
-},
-});
-
-} catch (error) {
-console.log(error);
-}
-
-info.innerHTML = `
-<img src="${resultCountry.data[0].flags.svg}" class="flag-img">
-<h2>${resultCountry.data[0].name.common}</h2>
-<div class="wrapper">
-    <div class="data-wrapper">
-        <h4>Capital:</h4>
-        <span>${resultCountry.data[0].capital[0]}</span>
-    </div>
-</div>
-<div class="wrapper">
-    <div class="data-wrapper">
-        <h4>Continent:</h4>
-        <span>${resultCountry.data[0].continents[0]}</span>
-    </div>
-</div>
- <div class="wrapper">
-    <div class="data-wrapper">
-        <h4>Population:</h4>
-        <span>${resultCountry.data[0].population}</span>
-    </div>
-</div>
-<div class="wrapper">
-    <div class="data-wrapper">
-        <h4>Currency:</h4>
-        <span>${
-          resultCountry.data[0].currencies[Object.keys(resultCountry.data[0].currencies)].name
-        } - ${Object.keys(resultCountry.data[0].currencies)[0]}</span>
-    </div>
-</div>
- <div class="wrapper">
-    <div class="data-wrapper">
-        <h4>Common Languages:</h4>
-        <span>${Object.values(resultCountry.data[0].languages)
-          .toString()
-          .split(",")
-          .join(", ")}</span>
-    </div>
-</div>
-<div class="wrapper">
-  <div class="data-wrapper">
-      <h4>Weather (&#x2103 C):</h4>
-      <span>${resultWeather.data.main.temp}</span>
-  </div>
-</div>
-<div class="wrapper">
-<div class="data-wrapper">
-    <h4>Wiki Links:</h4>
-    <br>
-    <span><a href="${resultWiki.data.geonames[0].wikipediaUrl}">${resultWiki.data.geonames[0].wikipediaUrl}</a></span>
-    <span><a href="${resultWiki.data.geonames[1].wikipediaUrl}">${resultWiki.data.geonames[1].wikipediaUrl}</a></span>
-    <span><a href="${resultWiki.data.geonames[2].wikipediaUrl}">${resultWiki.data.geonames[2].wikipediaUrl}</a></span>
-
-</div>
-</div>
-
-`;
-
-applyCountryBorder(map, inputLow);
-        
-      }})
-    })}
-
- else {
-  console.log("Geolocation is not supported by this browser.");
-}
+                url: "php/wikiLinks.php",
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    countryName: inputLow
+               },
+            });
+    
+     
+          resultWeather = await $.ajax({
+    
+                url: "php/openWeather.php",
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    lat: resultCountry.data[0].latlng[0],
+                    lon: resultCountry.data[0].latlng[1],
+                    apiKey: /*process.env.API_KEY*/ '527a25be90efba24541cd7ca1ac87e7e'
+                },
+            });
+        }
+ 
 
 
 
